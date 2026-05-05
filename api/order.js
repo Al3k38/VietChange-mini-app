0f4c570506364810467361c4b67307a801c1d021
Al3k38 <o.al3ksandr@gmail.com>
2026-05-06 01:16:57 +0900

    Fix formatting of exchange rate display

--- api/order.js
+++ api/order.js
@@ -104,7 +104,7 @@ function buildGroupMessage(d, orderNum) {
     `<b>Обмен: ${d.fromLabel} → ${d.toLabel}</b>`,
     `<b>Продажа:</b> <code>${d.amtFrom}</code> ${fromFlag} ${d.fromCode}`,
     `<b>Покупка:</b> <code>${d.amtTo}</code> ${toFlag} ${d.toCode}`,
-    `<pre>Курс: ${d.rate}     </pre>`,
+    `<pre>Курс: ${d.rate}  </pre>`,
     ``,
     `<b>Способ:</b> ${d.method}`,
     `<b>Дата:</b> ${d.date}`,
